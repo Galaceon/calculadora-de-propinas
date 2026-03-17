fe906f8b9f3e3069c0c4db769cd9332b1409db52
Galaceon <antoniogarcialtr@gmail.com>
2026-03-17 20:50:29 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-8xl font-light">Hola mundo</h1>
+      <h2 className="text-4xl">hola buenas</h2>
+      <h3>y ahora</h3>
+    </>
+  )
+}
+
+export default App
