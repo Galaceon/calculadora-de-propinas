a15dc9cf6937705512e99599032e9367c27cf70c
Galaceon <antoniogarcialtr@gmail.com>
2026-03-22 21:22:58 +0100

    calc total amount

--- src/components/OrderTotals.tsx
+++ src/components/OrderTotals.tsx
@@ -12,6 +12,7 @@ export default function OrderTotals({order, tip} : OrderTotalsProps) {
 
   const subtotalAmount = useMemo( () => order.reduce( ( total, item ) => total + ( item.quantity * item.price ), 0 ), [order] )
   const tipAmount = useMemo( () => subtotalAmount * tip, [tip, order] )
+  const totalAmount = useMemo( () => subtotalAmount + tipAmount , [order, tip] )
 
   return (
     <>
@@ -21,7 +22,7 @@ export default function OrderTotals({order, tip} : OrderTotalsProps) {
 
         <p>Propina: <span className="font-bold">{formatCurrency(tipAmount)}</span></p>
 
-        <p>Total a pagar: <span className="font-bold">0,00€</span></p>
+        <p>Total a pagar: <span className="font-bold">{formatCurrency(totalAmount)}</span></p>
       </div>
 
       <button></button>
